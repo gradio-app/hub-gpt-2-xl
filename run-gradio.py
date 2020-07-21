fdf91f16b5af533f8e70b5a3d48d39e1a5fcae4f
aliabd <ali.si3luwa@gmail.com>
2020-07-21 01:39:47 -0700

    reduced runtime, added exmaples

--- run-gradio.py
+++ run-gradio.py
@@ -11,11 +11,13 @@ def predict(inp, model_type):
     else:
         model, tokenizer = model_small, tokenizer_small
     input_ids = tokenizer.encode(inp, return_tensors='tf')
-    beam_output = model.generate(input_ids, max_length=100, num_beams=5,
+    beam_output = model.generate(input_ids, max_length=40, num_beams=5,
                                  no_repeat_ngram_size=2, early_stopping=True)
     output = tokenizer.decode(beam_output[0], skip_special_tokens=True,
                               clean_up_tokenization_spaces=True)
-    return ".".join(output.split(".")[:-1]) + "."
+    if output.count(".") >= 2:
+        output = ".".join(output.split(".")[:-1]) + "."
+    return output
 
  
 
@@ -25,17 +27,20 @@ INPUTS = [gradio.inputs.Textbox(lines=2, label="Input Text"),
                                                                 "and small")]
 OUTPUTS = gradio.outputs.Textbox()
 examples = [
-    ["The British government has secured early access to more than 90 "
-     "million vaccine doses.", "gpt2-xl"],
-    ["The three hardest things about software engineering are", "gpt2-xl"]
+    ["The toughest thing about software engineering is", "gpt2-xl"],
+    ["The future of AI ", "gpt2-xl"],
+    ["Is this the real life? Is this just fantasy?", "gpt2-small"]
 ]
 INTERFACE = gradio.Interface(fn=predict, inputs=INPUTS, outputs=OUTPUTS, title="GPT-2",
                  description="GPT-2 is a large transformer-based language "
                              "model with 1.5 billion parameters, trained on "
-                             "a dataset[1] of 8 million web pages. GPT-2 is "
+                             "a dataset of 8 million web pages. GPT-2 is "
                              "trained with a simple objective: predict the "
                              "next word, given all of the previous words "
-                             "within some text. You can configre small vs xl below (the xl model takes longer to run but generates better text.",
+                             "within some text. You can configure small vs "
+                             "xl below: the xl model takes longer to run ("
+                             "55s vs 15s) "
+                             "but generates better text.",
                  thumbnail="https://github.com/gradio-app/gpt-2/raw/master/screenshots/interface.png?raw=true",
                              examples=examples,
                  capture_session=False)
